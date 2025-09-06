5171e3ffa814fb21b6c90cb3ad645e70111e63a7
mkajla662-cloud <mkajla662@gmail.com>
2025-09-06 14:54:12 +0530

    Create modules/vars.py

---
+++ modules/vars.py
@@ -0,0 +1,10 @@
+# SudoR2spr WOODcraft
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+
+API_ID    = os.environ.get("API_ID", "29755489")
+API_HASH  = os.environ.get("API_HASH", "05e0d957751c827aa03494f503ab54fe")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
